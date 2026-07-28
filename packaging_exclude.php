18f4f626ff5d78183f9e7c18874737af533f38bb
Konrad Michalik <km@move-elevator.de>
2026-07-28 10:57:17 +0200

    chore: add root package metadata and community files

---
+++ packaging_exclude.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "typo3_routing" TYPO3 CMS extension.
+ *
+ * (c) 2026 Konrad Michalik <hej@konradmichalik.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'directories' => [
+        '.build',
+        '.ddev',
+        '.git',
+        '.github',
+        'bin',
+        'build',
+        'public',
+        'tailor-version-upload',
+        'tests',
+        'var',
+        'vendor',
+    ],
+    'files' => [
+        'DS_Store',
+        'CLAUDE.md',
+        'CODE_OF_CONDUCT.md',
+        'CODEOWNERS',
+        'composer.lock',
+        'composer-require-checker.json',
+        'composer-unused.php',
+        'CONTRIBUTING.md',
+        'editorconfig',
+        'gitattributes',
+        'gitignore',
+        'packaging_exclude.php',
+        'php-cs-fixer.php',
+        'phpstan-baseline.neon',
+        'phpunit.functional.xml',
+        'phpunit.xml',
+        'phpstan.neon',
+        'rector.php',
+        'renovate.json',
+        'version-bumper.yaml',
+    ],
+];
